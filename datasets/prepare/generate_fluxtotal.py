f4cd8e724670190039c8b9c9444d39def152fcbb
yu <jingaiyu@amd.com>
2026-01-27 11:08:27 +0800

    Update generate_fluxtotal.py
    add duplicate diffusiondb prompts

--- datasets/prepare/generate_fluxtotal.py
+++ datasets/prepare/generate_fluxtotal.py
@@ -239,8 +239,10 @@ def write_df(args: ArgumentParser ):
             for i in range(256):
                 pr_select.append(metadata_df['prompt'][i])
         elif args.download_mode == 'total':
-            for i in range(len(metadata_df)):
-                pr_select.append(metadata_df['prompt'][i])
+            metadata_df_select = metadata_df.drop_duplicates(subset=['prompt'], keep='first')
+            print('select prompt : ', len(metadata_df_select))
+            for i in range(len(metadata_df_select)):
+                pr_select.append(metadata_df_select['prompt'][i])
 
     bs=args.bs
     for i in range(0,len(pr_select)-len(pr_select)%bs, bs):
